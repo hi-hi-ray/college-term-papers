b95373e8f7eaaf49c6c4cff96b9decfff85aa7dd
Raysa Dutra <hi-hi-ray@users.noreply.github.com>
2017-06-12 15:12:27 -0300

    Add files via upload

---
+++ Bloco D - Desenvolvimento .Net/KeepEverything.RaysaDutra/KeepEverything.Consumer.RaysaDutra/KeepEverything.Consumer.RaysaDutra/Models/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeepEverything.Consumer.RaysaDutra.Models
+{
+    public class CategoryViewModel
+    {
+        public int IdCategory { get; set; }
+        public string NameCategory { get; set; }
+        public string DescriptionCategory { get; set; }
+    }
+}
---
+++ Bloco D - Desenvolvimento .Net/KeepEverything.RaysaDutra/KeepEverything.Consumer.RaysaDutra/KeepEverything.Consumer.RaysaDutra/Models/TagViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeepEverything.Consumer.RaysaDutra.Models
+{
+    public class TagViewModel
+    {
+        public int IdTag { get; set; }
+        public string NameTag { get; set; }
+    }
+}
