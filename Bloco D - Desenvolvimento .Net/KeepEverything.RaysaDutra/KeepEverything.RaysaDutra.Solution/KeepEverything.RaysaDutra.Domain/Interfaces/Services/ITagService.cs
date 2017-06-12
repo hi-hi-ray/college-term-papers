e17a15dde51608fa9f3aa2e76a701eb8f7a3a048
Raysa Dutra <hi-hi-ray@users.noreply.github.com>
2017-06-12 15:56:45 -0300

    Add files via upload

---
+++ Bloco D - Desenvolvimento .Net/KeepEverything.RaysaDutra/KeepEverything.RaysaDutra.Solution/KeepEverything.RaysaDutra.Domain/Interfaces/Services/ITagService.cs
@@ -0,0 +1,8 @@
+﻿using KeepEverything.RaysaDutra.Domain.Entities;
+
+namespace KeepEverything.RaysaDutra.Domain.Interfaces.Services
+{
+    public interface ITagService : IServiceBase<Tag>
+    {
+    }
+}
---
+++ Bloco D - Desenvolvimento .Net/KeepEverything.RaysaDutra/KeepEverything.RaysaDutra.Solution/KeepEverything.RaysaDutra.Domain/Services/ServiceBase.cs
@@ -0,0 +1,46 @@
+﻿using KeepEverything.RaysaDutra.Domain.Interfaces.Repositories;
+using KeepEverything.RaysaDutra.Domain.Interfaces.Services;
+using System.Collections.Generic;
+
+namespace KeepEverything.RaysaDutra.Domain.Services
+{
+    public class ServiceBase<T> : IServiceBase<T> where T : class
+    {
+        private readonly IRepositoryBase<T> _repositoryBase;
+
+        public ServiceBase(IRepositoryBase<T> repositoryBase)
+        {
+            _repositoryBase = repositoryBase;
+        }
+
+        public void Add(T Entity)
+        {
+            _repositoryBase.Add(Entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _repositoryBase.GetAll();
+        }
+
+        public T GetById(int? id)
+        {
+            return _repositoryBase.GetById(id);
+        }
+
+        public void Remove(T Entity)
+        {
+            _repositoryBase.Remove(Entity);
+        }
+
+        public void Remove(int? id)
+        {
+            _repositoryBase.Remove(id);
+        }
+
+        public void Update(T Entity)
+        {
+            _repositoryBase.Update(Entity);
+        }
+    }
+}
